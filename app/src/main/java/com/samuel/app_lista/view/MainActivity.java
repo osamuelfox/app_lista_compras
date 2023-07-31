3081c25b2c2a9c5d09c99657fb4bfb3a9417d6f9
osamuelfox <samuelsantosdesouza92@gmail.com>
2023-07-31 19:12:06 -0300

    Primeiro commit

---
+++ app/src/main/java/com/samuel/app_lista/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.samuel.app_lista.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.samuel.app_lista.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
